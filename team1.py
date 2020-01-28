b5373cb9cef0ae2647c7656559b9da34eb22efd8
Tyler Hendrickson <80806@GEHRHSD.LOCAL>
2020-01-28 11:54:47 -0500

    Update team1.py

--- team1.py
+++ team1.py
@@ -26,8 +26,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    if len(my_history)%2 == 0:
+        return 'c'
+    else:
+        return 'b'
+    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
