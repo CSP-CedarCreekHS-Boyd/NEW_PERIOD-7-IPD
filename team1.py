86fdded56fd0a0131d766ea1d410f214b15d2a2e
Tyler Hendrickson <80806@GEHRHSD.LOCAL>
2020-01-30 13:39:34 -0500

    Update team1.py

--- team1.py
+++ team1.py
@@ -14,9 +14,7 @@ def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
